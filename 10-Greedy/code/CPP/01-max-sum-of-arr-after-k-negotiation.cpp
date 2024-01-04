3549723f0a2b234abf6ff3520aa0f47bbf9ca24d
surjeetvermaiitm <surjeetverma2017@gmail.com>
2024-01-04 19:05:29 +0530

    greedy

---
+++ 10-Greedy/code/CPP/01-max-sum-of-arr-after-k-negotiation.cpp
@@ -0,0 +1,28 @@
+//Link: https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& nums, int k) {
+
+        int total=0;
+
+        for(int i=0;i<nums.size();i++){
+            total+=nums[i];
+        }
+
+        priority_queue<int,vector<int>,greater<int>> pq(nums.begin(),nums.end());//O(n)
+
+        // for(int i=0;i<nums.size();i++){
+        //     pq.push(nums[i]);
+        // }
+
+        while(k--){ //O(klogn + n)
+            int ele=pq.top();
+            pq.pop();
+            total -=ele;
+            pq.push(-1*ele);
+            total += -1*ele;
+        }
+
+        return total;
+    }
+};
