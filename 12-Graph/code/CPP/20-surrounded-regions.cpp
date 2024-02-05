9ef6688d038384f83412eb399045c578d2076259
surjeetvermaiitm <surjeetverma2017@gmail.com>
2024-02-05 16:07:02 +0530

    Graph

--- 12-Graph/code/CPP/20-surrounded-regions.cpp
+++ 12-Graph/code/CPP/20-surrounded-regions.cpp
@@ -1,28 +1,5 @@
-#### Q1. L-5 01 Matrix
+//Link: https://leetcode.com/problems/surrounded-regions/description/
 
-Link: https://leetcode.com/problems/01-matrix/description/
-Multisource BFS from 0
-
-```CPP
-
-```
-
-#### Q2. Minimum Knight move
-
-Link: https://leetcode.ca/all/1197.html
-
-BFS
-<i,j,step>
-
-```CPP
-
-```
-
-#### Q3. Surrounded regions
-
-Link: https://leetcode.com/problems/surrounded-regions/description/
-
-```CPP
 class Solution {
 public:
     vector<vector<int>> nbrs={{0,1},{0,-1},{1,0},{-1,0}};
@@ -35,7 +12,7 @@ public:
             if(x<0 || y<0 || x>=m || y>=n || visited[x][y] || board[x][y]=='X')
                 continue;
             dfs(board,x,y,visited);
-
+            
         }
     }
     void solve(vector<vector<char>>& board) {
@@ -66,21 +43,7 @@ public:
                 }
             }
         }
-
-
+        
+        
     }
 };
-```
-
-#### Q4. Walls and gates Premium
-
-Multi source BFS from Gates
-Link: https://www.lintcode.com/problem/663/
-
-#### Q5. Shortest Bridge
-
-distinguish both island multi source BFS from boundary of one islands
-
-Link: https://leetcode.com/problems/shortest-bridge/description/
-
-![1707129395312](image/05-graph-qn/1707129395312.png)
